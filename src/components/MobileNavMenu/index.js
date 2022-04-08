846e1958f7fca4cb95620539fba4c19a9f4dd2af
Bennett Dixon <benbendixon@gmail.com>
2022-04-08 15:58:45 -0700

    fix closing issue w/ current active route

--- src/components/MobileNavMenu/index.js
+++ src/components/MobileNavMenu/index.js
@@ -1,16 +1,21 @@
 import Link from 'next/link'
 import { navItems } from 'data'
 import * as S from './styles'
+import { useRouter } from 'next/dist/client/router'
 
 export default function MobileNavMenu ({ closeNav, open }) {
+  const router = useRouter()
+  const handleClick = (e, item) => {
+    e?.preventDefault()
+    router.push(item.link)
+    closeNav()
+  }
   return (
     <S.MobileNavMenu open={open}>
       <S.NavMenuItems>
         {navItems.map(item => (
           <S.NavItem>
-            <Link href={item.link} passHref>
-              <S.A onClick={closeNav}>{item.title}</S.A>
-            </Link>
+            <S.A onClick={e => handleClick(e, item)}>{item.title}</S.A>
           </S.NavItem>
         ))}
       </S.NavMenuItems>
